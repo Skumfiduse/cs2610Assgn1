1c323873c244b5ce058b59ad847d51c9a42351ed
Skumfiduse <grayson.champ3@gmail.com>
2025-01-28 18:08:59 -0700

    woked on server and parser

--- simpleHTTP/server.py
+++ simpleHTTP/server.py
@@ -34,6 +34,7 @@ from middleware import loggingMiddlewareFactory, notFoundMessageMiddlewareFactor
 # HTTP/1.1 200 Ok
 # HTTP/1.1 404 Not Found
 
+import socket
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind(("127.0.0.1", 8000))
@@ -41,26 +42,49 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print("listening on port 8000")
 
     while True:
-        print("connected: 1")
         connection, addr = s.accept()
         with connection:
-            print("connected: 2")
             data = connection.recv(8192)
-            print("connected: 3")
-
             if not data:
+                connection.close()
                 continue
-            print("connected: 4")
 
-            request = decode(data)
-            print(request.method)
-            print(request.uri)
-            print(request.version)
-            print(request.body)
-            print(request.headers)
-            middlewareChain = notFoundMessageMiddlewareFactory(router)
-            response = middlewareChain(request)
-            responseBytes = encode(response)
-            print("connected: 5")
+            # Parse the request
+            parsed = decode(data)
+            print(f"Method: {parsed.method}\nuri: {parsed.uri}\nversion: {parsed.version}\n body: {parsed.body}")
 
-            connection.send(responseBytes)
+            #TODO: parse the request, send through middleware and encode the response
+            res = "HTTP/1.1 200 Ok\nConnection: close\n\n<h1>Hello, world!</h1>"
+
+            connection.send(bytes(res, "UTF-8"))
+
+
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.bind(("127.0.0.1", 8000))
+#     s.listen()
+#     print("listening on port 8000")
+
+#     while True:
+#         print("connected: 1")
+#         connection, addr = s.accept()
+#         with connection:
+#             print("connected: 2")
+#             data = connection.recv(8192)
+#             print("connected: 3")
+
+#             if not data:
+#                 continue
+#             print("connected: 4")
+
+#             request = decode(data)
+#             print(request.method)
+#             print(request.uri)
+#             print(request.version)
+#             print(request.body)
+#             print(request.headers)
+#             middlewareChain = notFoundMessageMiddlewareFactory(router)
+#             response = middlewareChain(request)
+#             responseBytes = encode(response)
+#             print("connected: 5")
+
+#             connection.send(responseBytes)
